95e424d4544e89bc5e2016151595e38ce49cca84
monokoo <realstones2012@gmail.com>
2017-05-19 22:50:34 +0800

    added luci-app-timewol

---
+++ luci-app-timewol/files/usr/lib/lua/luci/controller/timewol.lua
@@ -0,0 +1,14 @@
+module("luci.controller.timewol",package.seeall)
+function index()
+if not nixio.fs.access("/etc/config/timewol")then
+return
+end
+entry({"admin","control","timewol"},cbi("timewol"),_("定时唤醒"),95).dependent=true
+entry({"admin","control","timewol","status"},call("status")).leaf=true
+end
+function status()
+local e={}
+e.timewol=luci.sys.call("cat /etc/crontabs/root |grep etherwake >/dev/null")==0
+luci.http.prepare_content("application/json")
+luci.http.write_json(e)
+end
