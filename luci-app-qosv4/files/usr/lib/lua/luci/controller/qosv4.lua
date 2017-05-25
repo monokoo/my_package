a02e9301e15653ca3f45ad15a84d4428f0dc5380
monokoo <realstones2012@gmail.com>
2017-05-25 13:58:55 +0800

    update

--- luci-app-qosv4/files/usr/lib/lua/luci/controller/qosv4.lua
+++ luci-app-qosv4/files/usr/lib/lua/luci/controller/qosv4.lua
@@ -1,28 +1,10 @@
---[[
-LuCI - Lua Configuration Interface
-
-Copyright 2011 Copyright 2011 flyzjhz <flyzjhz@gmail.com>
-
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-]]--
-
-module("luci.controller.qosv4", package.seeall)
-
+module("luci.controller.qosv4",package.seeall)
 function index()
-
-	if nixio.fs.access("/etc/config/qosv4") then
-	local page 
-	page = entry({"admin", "qos", "qosv4"}, cbi("qosv4"), _("QOSv4"),55)
-	page.i18n = "qosv4"
-	page.dependent = true
-	end
-
-	entry({"admin", "qos", "qosv4", "qosv4ip"}, cbi("qosv4ip"), nil).leaf = true
-
+if nixio.fs.access("/etc/config/qosv4")then
+local e
+e=entry({"admin","qos","qosv4"},cbi("qosv4"),_("QOSv4"),55)
+e.i18n="qosv4"
+e.dependent=true
+end
+entry({"admin","qos","qosv4","qosv4ip"},cbi("qosv4ip"),nil).leaf=true
 end
