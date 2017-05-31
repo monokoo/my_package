7d10d287c5061fc5e803255104ab742158df61ef
monokoo <realstones2012@gmail.com>
2017-05-31 15:03:37 +0800

    update ariang

---
+++ luci-app-ariang/files/www/web/scripts/config/languages.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    angular.module('ariaNg').constant('ariaNgLanguages', {
+        'en': {
+            name: 'English',
+            displayName: 'English'
+        },
+        'zh_CN': {
+            name: 'Simplified Chinese',
+            displayName: '简体中文'
+        }
+    });
+}());
