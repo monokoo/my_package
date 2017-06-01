796eebac6433cc87d0249292167d0f2bb0ca6fc8
monokoo <realstones2012@gmail.com>
2017-06-01 20:59:00 +0800

    update transations

--- luci-app-autoreboot/files/usr/lib/lua/luci/controller/autoreboot.lua
+++ luci-app-autoreboot/files/usr/lib/lua/luci/controller/autoreboot.lua
@@ -1,4 +1,4 @@
 module("luci.controller.autoreboot",package.seeall)
 function index()
-entry({"admin","system","autoreboot"},cbi("autoreboot"),_("定时重启"),88)
+entry({"admin","system","autoreboot"},cbi("autoreboot"),_("AutoReboot"),88)
 end
