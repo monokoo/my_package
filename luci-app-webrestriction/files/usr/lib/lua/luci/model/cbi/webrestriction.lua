041a65ac1ffb8df89f2f103767e242d31c361c14
monokoo <realstones2012@gmail.com>
2017-06-08 16:27:02 +0800

    fix netgear theme for koolss issue

--- luci-app-webrestriction/files/usr/lib/lua/luci/model/cbi/webrestriction.lua
+++ luci-app-webrestriction/files/usr/lib/lua/luci/model/cbi/webrestriction.lua
@@ -11,7 +11,7 @@ e=a:section(TypedSection,"basic",translate("全局设置"))
 e.anonymous=true
 t=e:option(Flag,"enable",translate("开启"))
 t.rmempty=false
-e=a:section(TypedSection,"macbind",translate("白名单设置"),translate("白名单中的客户端可以连接到互联网"))
+e=a:section(TypedSection,"whitelist","macbind",translate("白名单设置"),translate("白名单中的客户端可以连接到互联网"))
 e.template="cbi/tblsection"
 e.anonymous=true
 e.addremove=true
@@ -22,4 +22,17 @@ t.rmempty=true
 o.net.mac_hints(function(e,a)
 t:value(e,"%s (%s)"%{e,a})
 end)
+
+e=a:section(TypedSection,"blacklist","macbind",translate("黑名单设置"),translate("黑名单中的客户端被禁止连接到互联网"))
+e.template="cbi/tblsection"
+e.anonymous=true
+e.addremove=true
+t=e:option(Flag,"enable",translate("开启控制"))
+t.rmempty=false
+t=e:option(Value,"macaddr",translate("黑名单MAC地址"))
+t.rmempty=true
+o.net.mac_hints(function(e,a)
+t:value(e,"%s (%s)"%{e,a})
+end)
+
 return a
