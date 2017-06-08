d06aa68993c5784234d3d0eb716a6d98866e4062
monokoo <realstones2012@gmail.com>
2017-06-08 17:42:54 +0800

    update webrestriction

--- luci-app-webrestriction/files/usr/lib/lua/luci/model/cbi/webrestriction.lua
+++ luci-app-webrestriction/files/usr/lib/lua/luci/model/cbi/webrestriction.lua
@@ -1,6 +1,6 @@
 local o=require"luci.sys"
 local a,e,t
-a=Map("webrestriction",translate("访问限制"),translate("开启后默认禁止所有客户端连接到互联网，只有在白名单列表中的例外。"))
+a=Map("webrestriction",translate("访问限制"),translate("开启黑名单后禁止列表中的客户端连接到互联网，白名单暂时无法使用。"))
 a.template="webrestriction/index"
 e=a:section(TypedSection,"basic",translate("Running Status"))
 e.anonymous=true
@@ -11,19 +11,23 @@ e=a:section(TypedSection,"basic",translate("全局设置"))
 e.anonymous=true
 t=e:option(Flag,"enable",translate("开启"))
 t.rmempty=false
-e=a:section(TypedSection,"whitelist","macbind",translate("白名单设置"),translate("白名单中的客户端可以连接到互联网"))
-e.template="cbi/tblsection"
-e.anonymous=true
-e.addremove=true
-t=e:option(Flag,"enable",translate("开启控制"))
+t=e:option(ListValue,"limit_type",translate("限制类型"))
+t.default="blacklist"
+t:value("whitelist",translate("白名单"))
+t:value("blacklist",translate("Blacklist"))
 t.rmempty=false
-t=e:option(Value,"macaddr",translate("白名单MAC地址"))
-t.rmempty=true
-o.net.mac_hints(function(e,a)
-t:value(e,"%s (%s)"%{e,a})
-end)
-
-e=a:section(TypedSection,"blacklist","macbind",translate("黑名单设置"),translate("黑名单中的客户端被禁止连接到互联网"))
+-- e=a:section(TypedSection,"macbind",translate("白名单设置"),translate("白名单中的客户端可以连接到互联网"))
+-- e.template="cbi/tblsection"
+-- e.anonymous=true
+-- e.addremove=true
+-- t=e:option(Flag,"enable",translate("开启控制"))
+-- t.rmempty=false
+-- t=e:option(Value,"macaddr",translate("白名单MAC地址"))
+-- t.rmempty=true
+-- o.net.mac_hints(function(e,a)
+-- t:value(e,"%s (%s)"%{e,a})
+-- end)
+e=a:section(TypedSection,"macbind",translate("黑名单设置"),translate("黑名单中的客户端被禁止连接到互联网"))
 e.template="cbi/tblsection"
 e.anonymous=true
 e.addremove=true
