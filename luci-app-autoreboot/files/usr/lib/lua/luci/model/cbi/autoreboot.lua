c79220d8943b433ae268cdc69ff0c010b7c64037
monokoo <realstones2012@gmail.com>
2017-06-27 15:34:48 +0800

    update autoreboot with limited numbers

--- luci-app-autoreboot/files/usr/lib/lua/luci/model/cbi/autoreboot.lua
+++ luci-app-autoreboot/files/usr/lib/lua/luci/model/cbi/autoreboot.lua
@@ -11,7 +11,11 @@ week:value(e,translate("星期"..e))
 end
 week.default=0
 pass=s:option(Value,"minute",translate("分"))
+pass.datatype = "range(0,59)"
+pass.rmempty = false
 hour=s:option(Value,"hour",translate("时"))
+hour.datatype = "range(0,23)"
+hour.rmempty = false
 local e=luci.http.formvalue("cbi.apply")
 if e then
 io.popen("/etc/init.d/autoreboot restart")
