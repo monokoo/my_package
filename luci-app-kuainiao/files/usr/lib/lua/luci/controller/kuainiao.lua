9af7d45b7c0054f0aeed26e9ae23d4a4217104fc
monokoo <realstones2012@gmail.com>
2017-07-20 12:29:49 +0800

    update kuainiao

--- luci-app-kuainiao/files/usr/lib/lua/luci/controller/kuainiao.lua
+++ luci-app-kuainiao/files/usr/lib/lua/luci/controller/kuainiao.lua
@@ -1,26 +1,34 @@
 module("luci.controller.kuainiao", package.seeall)
 
 function index()
-     require("luci.i18n")
-     luci.i18n.loadc("setting")
     local fs = luci.fs or nixio.fs
     if not fs.access("/etc/config/kuainiao") then
 		return
 	end
 	
-	
-	local page = entry({"admin", "services", "kuainiao"}, cbi("kuainiao"), "迅雷快鸟")
-	page.i18n = "kuainiao"
+	local page = entry({"admin", "services", "kuainiao"}, cbi("kuainiao"), "KuaiNiao",9)
 	page.dependent = true
-	entry({"admin","services","kuainiao","status"},call("status")).leaf=true
+	entry({"admin","services","kuainiao","status"},call("kuainiao_status")).leaf=true
+	entry({"admin","services","kuainiao","dwonstatus"},call("down_status")).leaf=true
+	entry({"admin","services","kuainiao","upstatus"},call("up_status")).leaf=true
 end
-function status()
-local t=require"luci.sys"
-local e=require"luci.http"
-local a=require"luci.model.uci".cursor()
-local t={
-running=(t.call("pgrep /usr/bin/kuainiao > /dev/null")==0),
-}
-e.prepare_content("application/json")
-e.write_json(t)
+
+function kuainiao_status()
+local e={}
+e.running=luci.sys.call("pgrep /usr/bin/kuainiao > /dev/null")==0
+luci.http.prepare_content("application/json")
+luci.http.write_json(e)
+end
+
+function up_status()
+local e={}
+e.upstatus=luci.sys.exec("cat /usr/share/kuainiao/kuainiao_up_state")
+luci.http.prepare_content("application/json")
+luci.http.write_json(e)
+end
+function down_status()
+local e={}
+e.dwonstatus=luci.sys.exec("cat /usr/share/kuainiao/kuainiao_down_state")
+luci.http.prepare_content("application/json")
+luci.http.write_json(e)
 end
