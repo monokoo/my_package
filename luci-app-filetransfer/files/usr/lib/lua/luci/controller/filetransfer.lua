c549ea64e738d390eaa40fad43fae6e420d99de9
monokoo <realstones2012@gmail.com>
2017-09-05 14:04:48 +0800

    update menu order num

--- luci-app-filetransfer/files/usr/lib/lua/luci/controller/filetransfer.lua
+++ luci-app-filetransfer/files/usr/lib/lua/luci/controller/filetransfer.lua
@@ -1,4 +1,4 @@
 module("luci.controller.filetransfer",package.seeall)
 function index()
-entry({"admin","system","filetransfer"},cbi("updownload"),_("FileTransfer"),89)
+entry({"admin","system","filetransfer"},cbi("updownload"),_("FileTransfer"),52)
 end
