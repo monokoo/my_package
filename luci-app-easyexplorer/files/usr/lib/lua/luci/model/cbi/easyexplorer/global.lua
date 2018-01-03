8cd5c60f339fb256f3d5301999f8d438de6c06db
monokoo <realstones2012@gmail.com>
2018-01-03 12:13:26 +0800

    add easyexplorer

--- luci-app-easyexplorer/files/usr/lib/lua/luci/model/cbi/easyexplorer/global.lua
+++ luci-app-easyexplorer/files/usr/lib/lua/luci/model/cbi/easyexplorer/global.lua
@@ -1,6 +1,7 @@
 local e=require"nixio.fs"
 local e=luci.http
 local a,t,e
+local router_ip = luci.sys.exec("uci -q get network.lan.ipaddr")
 a=Map("easyexplorer",translate("easyexplorer"),translate("EasyExplorer是koolshare小宝开发的，支持跨设备、点对点文件传输同步工具。</br>你需要先到 https://ddns.to 注册，然后在本插件内填入Token和设置本地同步文件夹"))
 a.template="easyexplorer/index"
 t=a:section(TypedSection,"global",translate("Running Status"))
@@ -21,12 +22,21 @@ e.rmempty=true
 e=t:option(Value,"token",translate('ddnsto Token'))
 e.password=true
 e.rmempty=false
-if nixio.fs.access("/etc/config/easyexplorer")then
-e=t:option(Button,"Configuration",translate("域名配置管理"))
+local devices = {}
+nixio.util.consume((nixio.fs.glob("/tmp/mnt/sd?*")), devices)
+e=t:option(Value,"folder",translate('本地同步文件夹'))
+e.rmempty=false
+for i, dev in ipairs(devices) do
+        e:value(dev.."/easyexplorer")
+end
+e=t:option(Value,"folder",translate('本地同步文件夹'))
+e.rmempty=false
+if(luci.sys.call("pgrep /usr/bin/easyexplorer >/dev/null")==0)then
+e=t:option(Button,"Configuration",translate("WEB控制台"))
 e.inputtitle=translate("打开网站")
 e.inputstyle="reload"
 e.write=function()
-luci.http.redirect("https://www.ddns.to")
+luci.http.redirect("http:// .. router_ip .. :8899")
 end
 end
 return a
