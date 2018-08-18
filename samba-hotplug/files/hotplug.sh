ab86b0a909e7e65fa589561314aefdaceaaef414
monokoo <realstones2012@gmail.com>
2018-08-18 15:50:12 +0800

    fix issue with samba4

--- samba-hotplug/files/hotplug.sh
+++ samba-hotplug/files/hotplug.sh
@@ -13,40 +13,40 @@
 set_samba(){
 	section=$get_uuid
 
-	uci set samba.${section}="sambashare"
-	uci set samba.${section}.name="Disk_${device}"
-	uci set samba.${section}.path="${mountpoint}"
-	uci set samba.${section}.create_mask="0777"
-	uci set samba.${section}.dir_mask="0777"
-	uci set samba.${section}.read_only="no"
-	uci set samba.${section}.guest_ok="yes"
-	uci commit samba
+	uci set samba4.${section}="sambashare"
+	uci set samba4.${section}.name="Disk_${device}"
+	uci set samba4.${section}.path="${mountpoint}"
+	uci set samba4.${section}.create_mask="0777"
+	uci set samba4.${section}.dir_mask="0777"
+	uci set samba4.${section}.read_only="no"
+	uci set samba4.${section}.guest_ok="yes"
+	uci commit samba4
 }
 set_samba_path(){
 	section=$get_uuid
 
-	uci set samba.${section}.path="${mountpoint}"
-	uci commit samba
+	uci set samba4.${section}.path="${mountpoint}"
+	uci commit samba4
 }
 
 del_sambadir(){
-	mountdir=$(uci -q get samba.${s_uuid}.path)
+	mountdir=$(uci -q get samba4.${s_uuid}.path)
 	[ -z "`mount | grep '$mountdir'`" ] && {
 		#[ -d "$mountdir" ] && [ -z "`ls $mountdir`" ] && rm -rf $mountdir
-		uci del samba.${s_uuid}
-		uci commit samba
+		uci del samba4.${s_uuid}
+		uci commit samba4
 		logger -t Auto-Samba "The unused samba share uuid: $s_uuid has been removed."
 	}
 }
 
 remove_samba(){
-	samba_uuid=$(uci show samba |grep "=sambashare" | awk -F'.' '{print $2}'|awk -F'=' '{print $1}')
+	samba_uuid=$(uci show samba4 |grep "=sambashare" | awk -F'.' '{print $2}'|awk -F'=' '{print $1}')
 	[ -n "$samba_uuid" ] && {
 		for s_uuid in $samba_uuid
 		do
-			[ -n `cat /proc/self/mounts | grep -w "$(uci -q get samba.${s_uuid}.path)"` ] && continue
+			[ -n `cat /proc/self/mounts | grep -w "$(uci -q get samba4.${s_uuid}.path)"` ] && continue
 			del_sambadir
-			/etc/init.d/samba restart
+			/etc/init.d/samba4 restart
 		done
 	}
 }
@@ -65,19 +65,19 @@ case "$ACTION" in
 			get_uuid=`block info | grep -w "/dev/$device" | awk -F "UUID=" '{print $2}' | awk -F "\"" '{print $2}' | sed 's/-//g'`
 			[ -z "$get_uuid" ] && get_uuid=`blkid  /dev/$device | awk -F "UUID=" '{print $2}' | awk -F "\"" '{print $2}' | sed 's/-//g'`
 			[ -z "$get_uuid" ] && logger -t Auto-Samba "The new device /dev/${device} has no uuid!" && exit 1
-			have_uuid=$(uci show samba | grep -c "${get_uuid}=sambashare")
+			have_uuid=$(uci show samba4 | grep -c "${get_uuid}=sambashare")
 			[ "$have_uuid" = "0" ] && {
 				#remove_samba
 				set_samba
 				logger -t Auto-Samba "The new device /dev/${device} has been shared in $mountpoint ."
 			}
 			[ "$have_uuid" = "1" ] && {
-				[ "$(uci -q get samba.${get_uuid}.path)" != "$mountpoint" ] && {
+				[ "$(uci -q get samba4.${get_uuid}.path)" != "$mountpoint" ] && {
 					set_samba_path
 					logger -t Auto-Samba "The new device /dev/${device} has been shared in $mountpoint ."
 				}
 			}
-			/etc/init.d/samba restart
+			/etc/init.d/samba4 restart
 		}
 	;;
 	remove)
