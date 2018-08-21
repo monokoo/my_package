17f080b92a10def071da664ad116583ec5110e08
monokoo <realstones2012@gmail.com>
2018-08-21 23:58:01 +0800

    update

--- luci-app-advanced/files/usr/lib/lua/luci/model/cbi/advanced.lua
+++ luci-app-advanced/files/usr/lib/lua/luci/model/cbi/advanced.lua
@@ -1,21 +1,12 @@
 local e=require"nixio.fs"
 local t=require"luci.sys"
+local uci=luci.model.uci.cursor()
 m=Map("advanced",translate("高级设置"),translate("各类服务配置文档的直接编辑,除非你知道自己在干什么,否则请不要轻易修改这些配置文档，配置不正确可能会导致不能开机等严重错误。"))
 s=m:section(TypedSection,"advanced")
 s.anonymous=true
 s:tab("base",translate("Basic Settings"))
 o=s:taboption("base",Flag, "usb3_disable", translate("Switch off USB3.0"), translate("Turn off USB3.0, reduce effects of interference to 2.4G wireless."))
 o.default = 0
-o=s:taboption("base",ListValue, "webshell", translate("WebShell"), translate("Choose Which WebShell Service to Use"))
-o:value("ttyd",translate("ttyd"))
-o:value("shellinabox",translate("shellinabox"))
-o.default = "shellinabox"
-o=s:taboption("base",ListValue, "route_mode", translate("Operating Mode"), translate("Display partial data when APmode is enabled, Use Router Mode to disable AP mode."))
-o.default="none"
-o:value("none",translate("Current Mode"))
-o:value("apmode",translate("AP Mode"))
-o:value("dhcpmode",translate("Router Mode"))
-
 if(luci.sys.call("cat /etc/openwrt_release | grep DISTRIB_TARGET | grep -w bcm53xx >/dev/null")==0)then
 o=s:taboption("base",ListValue, "lan2wan", translate("LAN2WAN"), translate("Choose which lan port to be setted to another WAN port for Multi-ISP"))
 o:value("none",translate("Current Mode"))
@@ -25,7 +16,19 @@ o:value("2",translate("LAN3"))
 o:value("factory",translate("Back to Default"))
 o.default = "none"
 end
-
+rollbacktime = uci:get("luci", "apply", "rollback")
+o=s:taboption("base",Value, "rollback", translate("RollBack Time"), translate("Rollback time when luci update"))
+o.datatypes="and(uinteger,min(20))"
+o.default = rollbacktime
+o=s:taboption("base",ListValue, "webshell", translate("WebShell"), translate("Choose Which WebShell Service to Use"))
+o:value("ttyd",translate("ttyd"))
+o:value("shellinabox",translate("shellinabox"))
+o.default = "shellinabox"
+o=s:taboption("base",ListValue, "route_mode", translate("Operating Mode"), translate("Display partial data when APmode is enabled, Use Router Mode to disable AP mode."))
+o.default="none"
+o:value("none",translate("Current Mode"))
+o:value("apmode",translate("AP Mode"))
+o:value("dhcpmode",translate("Router Mode"))
 if nixio.fs.access("/etc/dnsmasq.conf")then
 s:tab("dnsmasqconf",translate("配置dnsmasq"),translate("本页是配置/etc/dnsmasq.conf的文档内容。编辑后点击重启按钮后生效"))
 
