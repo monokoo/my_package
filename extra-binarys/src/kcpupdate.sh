9a6a136f1913b992f5017624beeedbb155edf498
monokoo <monokoo@users.noreply.github.com>
2018-10-17 10:39:13 +0800

    update kcpupdate

--- extra-binarys/src/kcpupdate.sh
+++ extra-binarys/src/kcpupdate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 LOGFILE="/var/log/shadowsocks.log"
+LOCK_FILE=/var/lock/kcpupdate.lock
 reboot="0"
 
 echo_date(){
@@ -7,10 +8,28 @@ echo_date(){
 	echo $(date): "$log" >> $LOGFILE
 }
 
+set_lock(){
+	exec 1000>"$LOCK_FILE"
+	flock -x 1000
+}
+
+unset_lock(){
+	flock -u 1000
+	rm -rf "$LOCK_FILE"
+}
+
+_exit()
+{
+    local rc=$1
+    unset_lock
+    exit ${rc}
+}
+
 version=$(cat /etc/openwrt_release 2>/dev/null | grep -w DISTRIB_RELEASE | grep -w "By stones")
 version2=$(cat /etc/openwrt_release 2>/dev/null | grep -w DISTRIB_DESCRIPTION | grep -w Koolshare)
 [ -z "$version" -a -z "$version2" ] && exit 0
 
+set_lock
 echo_date "***********************************"
 echo_date "开始检测远程服务器最新版本..."
 local_version=`/usr/bin/kcpclient -v | awk '{print $3}'`
@@ -25,7 +44,7 @@ latest_version=`curl -S  https://github.com/xtaci/kcptun/releases/latest | awk -
 if [ -n "$latest_version" ]; then
 	echo_date "远程 KCP 最新版本：v$latest_version"
 else
-	echo_date "未检测到远程服务器 KCP 客户端最新版本，请稍候重试!" && exit 0
+	echo_date "未检测到远程服务器 KCP 客户端最新版本，请稍候重试!" && _exit 0
 fi
 
 arch=`uname -m`
@@ -45,18 +64,26 @@ if [ "$latest_version" != "$local_version" ]; then
 	echo_date "开始更新本地 KCP 客户端..."
 	mkdir -p /tmp/kcptun-linux-$s_arch
 	/usr/bin/wget --no-check-certificate --timeout=8 -t 2 https://github.com/xtaci/kcptun/releases/download/v$latest_version/kcptun-linux-$s_arch-$latest_version.tar.gz -O /tmp/kcptun-linux-$s_arch/kcptun-linux-$s_arch-$latest_version.tar.gz
-	[ ! -s "/tmp/kcptun-linux-$s_arch/kcptun-linux-$s_arch-$latest_version.tar.gz" ] && echo_date "下载失败，请稍候重试！" && exit 0
-	tar -xzf /tmp/kcptun-linux-$s_arch/kcptun-linux-$s_arch-$latest_version.tar.gz -C /tmp/kcptun-linux-$s_arch
-	mv /tmp/kcptun-linux-$s_arch/client_linux_$client_arch /usr/bin/kcpclient && chmod +x /usr/bin/kcpclient && {
+	[ ! -s "/tmp/kcptun-linux-$s_arch/kcptun-linux-$s_arch-$latest_version.tar.gz" ] && echo_date "下载失败，请稍候重试！" && _exit 0
+	tar -xzf /tmp/kcptun-linux-$s_arch/kcptun-linux-$s_arch-$latesV2Rayt_version.tar.gz -C /tmp/kcptun-linux-$s_arch
+	mv /tmp/kcptun-linux-$s_arch/client_linux_$client_arch /usr/bin/kcpclient && chmod +x /usr/bin/kcpclient
+	if [ "$?" -eq 0 ]; then
 		echo_date "==================================="
 		echo_date "本地 KCP 客户端更新成功"
-	}
+	else
+		echo_date "==================================="
+		echo_date "本地 KCP 客户端更新失败，请稍候再试！"
+		rm -rf /tmp/kcptun-linux-$s_arch
+		_exit 0
+	fi
 	rm -rf /tmp/kcptun-linux-$s_arch
 	reboot="1"
 else
 	echo_date "==================================="
 	echo_date "本地KCP客户端已经是最新版本了，无需更新！"
 fi
+
+unset_lock
 
 if [ "$reboot" == "1" ];then
 	[ -f "/var/lock/shadowsocks.lock" ] && exit 0
