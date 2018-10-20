917c3e47835d1877f3fd267ccf4270bd527df256
monokoo <monokoo@users.noreply.github.com>
2018-10-20 13:31:08 +0800

    update

--- extra-binarys/src/addroute.sh
+++ extra-binarys/src/addroute.sh
@@ -11,13 +11,13 @@ version=$(cat /etc/openwrt_release 2>/dev/null | grep -w DISTRIB_RELEASE | grep 
 version2=$(cat /etc/openwrt_release 2>/dev/null | grep -w DISTRIB_DESCRIPTION | grep -w Koolshare)
 [ -z "$version" -a -z "$version2" ] && exit 0
 
-if [ $wanport -gt 0 ] && [ -z $(/usr/sbin/ip route show|grep $routeip) ];then
-  gateway=$(/usr/sbin/ip route show|grep default|awk -F " " '{print $3}'|sed -n $wanport"p")
-  devname=$(/usr/sbin/ip route show|grep default|awk -F " " '{print $5}'|sed -n $wanport"p")
+if [ $wanport -gt 0 ] && [ -z $(ip route show|grep $routeip) ];then
+  gateway=$(ip route show|grep default|awk -F " " '{print $3}'|sed -n $wanport"p")
+  devname=$(ip route show|grep default|awk -F " " '{print $5}'|sed -n $wanport"p")
   epname=$(cat /var/state/network|grep -w $devname|awk -F'.' '{print $2}')
   [ -z "$epname" ] && epname=`echo $devname | awk -F'-' '{print $2}' 2>/dev/null`
   if [ -n $gateway ] && [ -n $devname ];then
-    /usr/sbin/ip route add $routeip via $gateway dev $devname >/dev/null 2>&1 &
+    ip route add $routeip via $gateway dev $devname >/dev/null 2>&1 &
     echo "$(date): 设置指定IP：$routeip 出口为：$epname"
 	if [ ! -f $cleanfile ];then
 		cat > $cleanfile <<EOF
